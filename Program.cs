58099d3683129ed6c69f83a865930e089a2d4d32
matheus sanderhus <sanderhus15@outlook.com>
2026-02-02 16:20:11 -0300

    Adicionando soma

--- Program.cs
+++ Program.cs
@@ -7,10 +7,20 @@ namespace Calculator
         static void Main(string[] args)
         {
             Console.Clear();
+
             Console.WriteLine("Primeiro valor: ");
             float v1 = float.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Segundo Valor: ");
+            float v2 = float.Parse(Console.ReadLine());
 
-            Console.WriteLine(v1);
+            Console.WriteLine("");
+
+            float resultado = v1 + v2;
+
+            // Console.WriteLine("O resultado da soma é " + resultado);
+            // Console.WriteLine($"O resultado da soma é {v1 + v2}");
+            Console.WriteLine($"O resultado da soma é {resultado}");
         }
     }
 }
