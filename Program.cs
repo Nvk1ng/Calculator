b9d8936c88b9b6b7a980fcfcd852a5e8415c1406
matheus sanderhus <sanderhus15@outlook.com>
2026-02-03 12:51:39 -0300

    Adicionand funcao subtracao

--- Program.cs
+++ Program.cs
@@ -28,5 +28,21 @@ namespace Calculator
             // Console.WriteLine($"O resultado da soma é {v1 + v2}");
             Console.WriteLine($"O resultado da soma é {resultado}");
         }
+
+        static void Subtracao()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Segundo Valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float resultado = v1 - v2;
+            Console.WriteLine($"O resultado da subtraçao é {resultado}");
+        }
     }
 }
